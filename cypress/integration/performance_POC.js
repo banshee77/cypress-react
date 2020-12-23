6724a951f7594005baef502186c9fb4a91a74d3c
Pedzich, Wieslaw <Wieslaw.Pedzich@sabre.com>
2020-12-23 15:04:40 +0100

    adding new test

--- cypress/integration/performance_POC.js
+++ cypress/integration/performance_POC.js
@@ -7,5 +7,12 @@ describe("REST API Tests", () => {
           expect(response.body).to.have.property('total', 12)
         })
     })
+
+
+    it("Vefiry that there are 12 elements in total", () => {
+      cy.request('https://reqres.in/api/users?delay=3')
+        .then((response) => {
+          expect(response.body).to.have.property('total', 12)
+        })
+    })
   })
-  
